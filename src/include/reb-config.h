a0f9fd32e3597618a9ac406ccd6012102a73c64b
ddharing <3249191+ddharing@users.noreply.github.com>
2019-06-22 09:03:06 -0400

    Added Endian DEFINE for Linux_X64

--- src/include/reb-config.h
+++ src/include/reb-config.h
@@ -150,6 +150,11 @@ These are now obsolete (as of A107) and should be removed:
 #define HAS_LL_CONSTS
 #endif
 
+#ifdef TO_LINUX_X64				// Linux/Intel
+#define ENDIAN_LITTLE
+#define HAS_LL_CONSTS
+#endif
+
 #ifdef TO_LINUX_PPC				// Linux/PPC
 #define ENDIAN_BIG
 #define HAS_LL_CONSTS
